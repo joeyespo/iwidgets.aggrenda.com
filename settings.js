bd72602515961579f9c216fd85a197c754d5b8bd
Joe Esposito <joe@joeyespo.com>
2014-06-08 02:26:48 -0400

    Take config from ENV (and the right ENV).

--- settings.js
+++ settings.js
@@ -2,6 +2,6 @@ module.exports = {
   HOST: process.env.HOST || null,
   PORT: process.env.PORT || 5000,
   DEFAULT_SHOT_WIDTH: process.env.DEFAULT_SHOT_WIDTH || 768,
-  DEFAULT_SHOT_MIN_HEIGHT: process.env.DEFAULT_SHOT_WIDTH || 700,
-  WEBSHOTS_QUALITY: 75,
+  DEFAULT_SHOT_MIN_HEIGHT: process.env.DEFAULT_SHOT_MIN_HEIGHT || 700,
+  WEBSHOTS_QUALITY: process.env.WEBSHOTS_QUALITY || 75,
 };
